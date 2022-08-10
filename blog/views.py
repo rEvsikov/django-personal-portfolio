f1cac4570bae7a0ff7052e9a88bba210f144465b
Ruslan <revsikov@ya.ru>
2022-08-10 21:32:34 +0300

    Created blog page with 3 sample blogs

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+def all_blogs(request):
+    posts = Blog.objects.all()
+    return render(request, 'blog/all_blogs.html', {'posts': posts})
