3f9dd2d977280debfbef2315665881f4b0325f01
Ruslan <revsikov@ya.ru>
2022-08-12 15:05:28 +0300

    Add template for blog-page, some new features to another pages.

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,7 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    date = models.DateField()
+    date = models.DateTimeField()
     
+    def __str__(self) -> str:
+        return self.title + ' (created ' + str(self.date) + ')'
